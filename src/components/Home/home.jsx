0e2bfd59097a94f343d931dc8973e6c5375796f8
bobofishstix <tynerbrad0313@gmail.com>
2021-05-13 11:13:55 -0500

    Added Home

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './home.css';
+class Home extends Component {
+    state = {  }
+    render() { 
+        return (<div className='test'>Hello World!</div>);
+    }
+}
+ 
+export default Home;
