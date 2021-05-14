a3863df02310dc49a0e1a5704dcad2c724984e54
bobofishstix <tynerbrad0313@gmail.com>
2021-05-14 15:24:06 -0500

    Added NavBar to COmponents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/home.jsx';
+import NavBar from './components/NavBar/navbar.jsx';
 // C:\Users\btyne\Documents\GitHub\widgets-demo\src\components\Home\home.jsx
 
 
