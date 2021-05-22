cb647acc64015c31c26868c688d8e462f1490dd3
bobofishstix <tynerbrad0313@gmail.com>
2021-05-22 04:47:03 -0500

    Created Magic 8 Ball

---
+++ src/components/EightBall/eightBall.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './eightBall.css';
+
+const quotes = [
+    'Yes',
+    'No',
+    'Maybe'
+];
+
+export default function App() {
+    const [question, setQuestion] = useState('');
+    const [quote, setQuote] = useState('');
+
+
+    const getQuote = (e) => {
+        e.preventDefault();
+        if (!question) {
+            return;
+        }
+        const index = Math.floor(Math.random() * quotes.length);
+        setQuote(quotes[index]);
+    };
+    return(
+        <div>
+            <form onSubmit={getQuote}>
+                <div>
+                    <label>
+                        What do you wish to know?
+                    </label>
+                    <br></br>
+                    <input value={question} placeholder='Ask Away'
+                            onChange={(e) => setQuestion(e.target.value)} />
+                    
+                    
+                </div>
+                    <button type="submit">
+                        Discover the Truth
+                    </button>
+            </form>
+            <div className="ball">
+                <p>{quote}</p>
+            </div>
+                
+
+        </div>
+    );
+}
--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,16 +1,19 @@
 import './navbar.css';
+import Ball from'C:/Users/btyne/Documents/GitHub/widgets-demo/src/components/EightBall/eightBall.jsx';
 import React, {  } from 'react';
 const NavBar = () => {
     return (
     <div className='navbarContainer'>
      <div className='row'>
-        <div className='widget-box col-sm'id='widget-box-1'>
-           This is a test
+        <div className='col-2'></div>
+        <div className='widget-box col-3'id='widget-box-1'>
+           <label> Magic 8 Ball</label>
+           <Ball> </Ball>
         </div>
-        <div className='widget-box col-sm'id='widget-box-2'>
+        <div className='widget-box col-3'id='widget-box-2'>
            This is also a test
         </div>
-        <div className='widget-box col-sm'id='widget-box-3'>
+        <div className='widget-box col-3'id='widget-box-3'>
             So is this
         </div>
      </div>   
