84ebe456701f4a8f2d045cbfe014011a8dcdd563
bobofishstix <tynerbrad0313@gmail.com>
2021-05-22 18:17:45 -0500

    Added formatting

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -5,18 +5,19 @@ const NavBar = () => {
     return (
     <div className='navbarContainer'>
      <div className='row'>
-        <div className='col-2'></div>
+        <div className='col-1'></div>
         <div className='widget-box col-3'id='widget-box-1'>
            <label> Magic 8 Ball</label>
            <Ball> </Ball>
         </div>
-        <div className='widget-box col-3'id='widget-box-2'>
+        <div className='widget-box col-4'id='widget-box-2'>
            This is also a test
         </div>
         <div className='widget-box col-3'id='widget-box-3'>
             So is this
         </div>
-     </div>   
+        <div className='col-1'></div>
+     </div>
     </div>
     );
 }
