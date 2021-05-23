a8033b292bd03869ddfb0163bb1b780f338c9e2d
bobofishstix <tynerbrad0313@gmail.com>
2021-05-23 16:29:46 -0500

    Minor cleanup

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,7 +1,4 @@
 import "./navbar.css";
-import Ball from "../EightBall/eightBall.jsx";
-import Mine from "../Minesweeper/minesweeper.jsx";
-import TTT from "../TTT/ttt.jsx";
 import React from "react";
 import placeholder from "./placeholder.jpg";
 const NavBar = () => {
@@ -12,21 +9,18 @@ const NavBar = () => {
         <a href="/ball">
           <img className="widget-preview" src={placeholder} alt="" />
         </a>
-        {/* <Ball> </Ball>  */}
       </div>
       <div className="widget-box">
         <header className="title"> Tic-Tac-Toe</header>
         <a href="/ttt">
           <img className="widget-preview" alt="" src={placeholder} />
         </a>
-        {/* <TTT></TTT> */}
       </div>
       <div className="widget-box">
         <header className="title"> Minesweeper</header>
         <a href="/minesweeper">
           <img className="widget-preview" alt="" src={placeholder} />
         </a>
-        {/* <Mine></Mine> */}
       </div>
     </div>
   );
